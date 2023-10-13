be6b4609c6b712c077914ff3c4b58e8f9d485ffd
ohseungmin <ohmin2886@gmail.com>
2023-10-13 11:04:12 +0900

    createserver 속 if문 작성

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-// 내장함수 http를 요구했다.
+// 내장모듈 http를 요구했다.
 const http = require('http');
 
+// 화살표 함수를 사용했다는 점을 들어 function을 작성하지 않았다.
+const server = http.createServer( (request, response) => {
+  if (request.method === "GET" && request.url === "/") {
+    response.writeHead(200, {"Content-Type": "text/html"})
+    response.end("hello");
+  }
+})

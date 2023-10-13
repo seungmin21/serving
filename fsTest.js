b6fae27d701502e588cbbc8e8e21c442098a1aab
ohseungmin <ohmin2886@gmail.com>
2023-10-13 11:52:35 +0900

    fs readfile 정상 작동 확인

--- fsTest.js
+++ fsTest.js
@@ -2,9 +2,12 @@
 const fs = require('fs');
 
 // 파일 읽기 file [option] callback
-fs.readFile('index.html', (err, data) => {
+// 6번 내용이 방식이 몇개 더 있을거라 판단
+// 혹시 몰라 직접 index 파일을 만들어서 테스트
+fs.readFile('index.html', 'utf8', (err, data) => {
   if (err) {
-
+    // throw 예외 발생 장치
+    throw err
   } else {
     console.log(data);
   }

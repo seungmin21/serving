5581278c69bd761951dd1b386c6e8a4b1601350b
ohseungmin <ohmin2886@gmail.com>
2023-10-13 12:37:35 +0900

    createserver에 fs.readfile을 적용 정상 작동 확인

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const http = require('http');
 // 화살표 함수를 사용했다는 점을 들어 function을 작성하지 않았다.
 const server = http.createServer( (request, response) => {
   if (request.method === "GET" && request.url === "/") {
+    
+    const fs = require('fs');
+
+    fs.readFile('index.html', 'utf8', (err, data) => {
+      if (err) {
+        throw err
+      } else {
+          console.log(data);
+        }
+    })
     response.writeHead(200, {"Content-Type": "text/html; charset = utf-8"})
     response.write("hello");
     response.end();

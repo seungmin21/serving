c2d80e2dbaf6d616ba3e6f83da8a45e1ca1f03df
ohseungmin <ohmin2886@gmail.com>
2023-10-13 11:06:31 +0900

    response 작성법 수정 중

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const http = require('http');
 // 화살표 함수를 사용했다는 점을 들어 function을 작성하지 않았다.
 const server = http.createServer( (request, response) => {
   if (request.method === "GET" && request.url === "/") {
-    response.writeHead(200, {"Content-Type": "text/html"})
-    response.end("hello");
+    response.writeHead(200, {"Content-Type": "text/html; charset = utf-8"})
+    response.write("hello");
+    response.end();
   }
 })
 server.listen(8080);

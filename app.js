9cf3575cb3750cea286b5be4f5ce55da4a5397ae
ohseungmin <ohmin2886@gmail.com>
2023-10-13 17:28:39 +0900

    상수 fs 위치 이동

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 // 내장모듈 http를 요구했다.
 const http = require('http');
+const fs = require('fs');
 
 // 화살표 함수를 사용했다는 점을 들어 function을 작성하지 않았다.
 const server = http.createServer( (request, response) => {
   if (request.method === "GET" && request.url === "/") {
     
-    const fs = require('fs');
-
     fs.readFile('index.html', 'utf8', (err, data) => {
       if (err) {
         throw err

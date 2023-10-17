bb1bedeff200a4be9fa5dfa21f581615de0b2577
kimwoojin123 <kimwoojin1903@gmail.com>
2023-10-17 14:29:00 +0900

    서버 작성 구문 수정

--- app.js
+++ app.js
@@ -3,25 +3,22 @@ const http = require('http');
 const fs = require('fs');
 
 // 화살표 함수를 사용했다는 점을 들어 function을 작성하지 않았다.
-const server = http.createServer( (request, response) => {
-  if (request.method === "GET" && request.url === "/") {
-    
-    fs.readFile('index.html', 'utf8', (err, data) => {
-      if (err) {
-        throw err
-      } else {
-          console.log(data);
-        }
-    })
-    response.writeHead(200, {"Content-Type": "text/html; charset = utf-8"})
-    response.write("hello");
-    response.end();
-  } else {
-    // 불일치에 대해서 오류문구 작성
-    response.writeHead(404, {"Content-Type": "text/html; charset = utf-8"})
-    response.write("404 페이지를 찾을 수 없습니다.")
-    response.end();
-  }
-})
+const server = http.createServer((request, response) => {
+    if (request.method === 'GET' && request.url === '/') {
+        fs.readFile('index.html', 'utf8', (err, data) => {
+            if (err) {
+                throw err;
+            } else {
+                response.writeHead(200, { 'Content-Type': 'text/html; charset = utf-8' });
+                response.end(data);
+                console.log(data);
+            }
+        });
+    } else {
+        // 불일치에 대해서 오류문구 작성
+        response.writeHead(404, { 'Content-Type': 'text/html; charset = utf-8' });
+        response.end('404페이지를 찾을 수 없습니다.');
+    }
+});
 // localhost:8080
 server.listen(8080);

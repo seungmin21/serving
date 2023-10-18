bcb8736c4505574a9116fd7207a6a4b1f3b5a059
ohseungmin <ohmin2886@gmail.com>
2023-10-18 18:28:37 +0900

    응답 수정

--- app.js
+++ app.js
@@ -4,23 +4,19 @@ const fs = require('fs');
 
 // 화살표 함수를 사용했다는 점을 들어 function을 작성하지 않았다.
 const server = http.createServer( (request, response) => {
+  // 요청 메서드를 GET(데이터 요청) 그리고 요청 url을 /(경로) 일치할 경우
   if (request.method === "GET" && request.url === "/") {
-    
     fs.readFile('index.html', 'utf8', (err, data) => {
       if (err) {
-        throw err
+        response.writeHead(404, {"Content-Type": "text/html; charset = utf-8"})
+        response.write("404 페이지를 찾을 수 없습니다.")
+        response.end(err);
       } else {
-          console.log(data);
+        response.writeHead(200, {"Content-Type": "text/html; charset = utf-8"})
+        response.write(data);
+        response.end();
         }
     })
-    response.writeHead(200, {"Content-Type": "text/html; charset = utf-8"})
-    response.write("hello");
-    response.end();
-  } else {
-    // 불일치에 대해서 오류문구 작성
-    response.writeHead(404, {"Content-Type": "text/html; charset = utf-8"})
-    response.write("404 페이지를 찾을 수 없습니다.")
-    response.end();
   }
 })
 // localhost:8080
